9835a690d72366c45e803c34bcd4b3448f2861bd
Brandon Manke <manke.brandon@gmail.com>
2018-11-22 01:51:41 -0600

    added: basic websocket impl to client
    also renamed index.html to example_index.html in subscriber

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API
+const socket = new WebSocket('ws://localhost:8080/ws');
+console.log(socket);
+
+socket.onopen = (evt) => {
+    console.log("OPEN");
+    socket.send('echo test');
+}
+
+socket.onclose = (evt) => {
+    console.log("CLOSE");
+}
+
+socket.onmessage = (evt) => {
+    console.log("RESPONSE: " + evt.data);
+}
+
+socket.onerror = (evt) => {
+    console.log("ERROR: " + evt.data);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
