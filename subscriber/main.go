f26233523f424ddff71fe049d1f9d1f12407fc4f
Brandon Manke <manke.brandon@gmail.com>
2018-11-22 00:59:23 -0600

    removed: golang socket.io library (outdated)

--- subscriber/main.go
+++ subscriber/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	socketio "github.com/googollee/go-socket.io"
 )
 
 func testSub() {
@@ -26,24 +25,6 @@ func main() {
 
 	go testSub()
 
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
-		panic(err)
-	}
-
-	server.On("connection", func(socket socketio.Socket) {
-		log.Println("connected!")
-		socket.On("disconnection", func() {
-			log.Println("disconnected..")
-		})
-	})
-
-	server.On("error", func(s socketio.Socket, err error) {
-		log.Println("error:", err)
-	})
-
-	http.Handle("/ws", server)
 	log.Println("Serving at localhost:8080...")
 	http.ListenAndServe(":8080", nil)
 }
