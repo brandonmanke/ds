440e81986ea50aab4d9564973386ea24d932fea2
Brandon Manke <manke.brandon@gmail.com>
2018-11-22 01:55:47 -0600

    remove: serviceWorker.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 // https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API
 const socket = new WebSocket('ws://localhost:8080/ws');
@@ -26,8 +25,3 @@ socket.onerror = (evt) => {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
