751eb5504ba52691afd8475e13b2ddd666d1e821
Brandon Manke <manke.brandon@gmail.com>
2018-11-21 23:55:52 -0600

    fixed: sub server port 80 -> port 8080

--- subscriber/main.go
+++ subscriber/main.go
@@ -44,6 +44,6 @@ func main() {
 	})
 
 	http.Handle("/ws", server)
-	log.Println("Serving at localhost:80...")
-	http.ListenAndServe(":80", nil)
+	log.Println("Serving at localhost:8080...")
+	http.ListenAndServe(":8080", nil)
 }
