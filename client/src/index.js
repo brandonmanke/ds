9a5b8dc021d9ee8b28c5526dd5f0781d183704c7
Rob Putyra <rpputyra@gmail.com>
2018-12-03 02:24:00 -0600

    sockets ready to handle test data from subscriber

--- client/src/index.js
+++ client/src/index.js
@@ -3,25 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
 // https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API
-const socket = new WebSocket('ws://localhost:8080/ws');
-console.log(socket);
+// const socket = new WebSocket('ws://localhost:8080/ws');
+// console.log(socket);
 
-socket.onopen = (evt) => {
-    console.log("OPEN");
-    socket.send('echo test');
-}
+// socket.onopen = (evt) => {
+//     console.log("OPEN");
+//     socket.send('echo test');
+// }
 
-socket.onclose = (evt) => {
-    console.log("CLOSE");
-}
+// socket.onclose = (evt) => {
+//     console.log("CLOSE");
+// }
 
-socket.onmessage = (evt) => {
-    console.log("RESPONSE: " + evt.data);
-}
+// socket.onmessage = (evt) => {
+//     console.log("RESPONSE: " + evt.data);
+// }
 
-socket.onerror = (evt) => {
-    console.log("ERROR: " + evt.data);
-}
+// socket.onerror = (evt) => {
+//     console.log("ERROR: " + evt.data);
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
